43076d03c69f42bf0720d40f4937bcd9ac940e18
leurosa <49928102+leurosa@users.noreply.github.com>
2025-06-13 13:54:55 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -103,8 +103,7 @@ def gerar_grafico(df, colunas, rpm_col="RPM", lambda_col="Lambda 1"):
             hover_template = (
                 f"<b>{c}</b><br>"
                 "Real: %{customdata:.3f}<br>"
-                "Plotado: %{y:.0f}<extra></extra>"
-            )
+                           )
         else:
             y_plot = real
             hover_template = f"<b>{c}</b><br>Valor: %{{y}}<extra></extra>"
